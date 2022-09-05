b6ed3196823f07f57c409fa5dbdd0ff0c0e398a3
Sudhanshu-Sathawane <110605235+Sudhanshu-Sathawane@users.noreply.github.com>
2022-09-05 10:34:59 +0530

    Ques2

---
+++ SwaggerAssignment/src/main/java/com/example/demo/SwaggerAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerAssignmentApplication.class, args);
+	}
+
+}
---
+++ SwaggerAssignment/src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,63 @@
+package com.example.demo.model;
+
+public class Product {
+	private String id;
+	private String name;
+	private int price;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + price;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (price != other.price)
+			return false;
+		return true;
+	}
+	
+	
+}
